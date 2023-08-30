64285d5bbc2373beead910138ec350d14b7e7508
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-08-30 08:45:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 Array.prototype.myMap = function(callback) {
-    
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this));
+    }
+    return newArray;
   };
   
   Array.prototype.myFilter = function(callback) {
-  
+    const newArray = [];
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        newArray.push(this[i]);
+      }
+    }
+    return newArray;
   };
   
   Array.prototype.myReduce = function(callback, initialValue) {
-
+    let accumulator = initialValue !== undefined ? initialValue : this[0];
+    for (let i = initialValue !== undefined ? 0 : 1; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+    return accumulator;
   };
   
   
